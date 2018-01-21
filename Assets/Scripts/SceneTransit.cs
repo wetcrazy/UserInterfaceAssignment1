0f25cb031ae184b90d158b7da96c44cbacb3d0c7
Isaac Choo <isaacchoo_jl@hotmail.com>
2018-01-21 22:15:09 +0800

    added my scenes and added scene transit script

---
+++ Assets/Scripts/SceneTransit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransit : MonoBehaviour {
+
+    public void Game()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene("Level Select");
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void LoadingScreen()
+    {
+        SceneManager.LoadScene("Loading Screen");
+    }
+}
