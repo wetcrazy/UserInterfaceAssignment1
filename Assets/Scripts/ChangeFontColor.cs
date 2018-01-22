17e78969d821d9219719675d13c1ff64bad3337f
RJHere <greennexus51@gmail.com>
2018-01-22 08:55:16 +0800

    Ui can change color on click , and changed buttons . loading bar in progress

---
+++ Assets/Scripts/ChangeFontColor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ChangeFontColor : MonoBehaviour {
+
+    public Text t;
+
+    public void ChangeColour()
+    {
+        t.color = Color.white;
+    }
+}
---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LoadingScreen : MonoBehaviour {
+
+    public GameObject loadingscreen;
+    public Slider slider;
+
+	public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingscreen.SetActive(true);
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            yield return null;
+        }
+    }
+  
+}
