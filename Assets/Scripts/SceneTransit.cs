79b84c625e241fa2161a47cd168671a1e8adadd6
Isaac Choo <isaacchoo_jl@hotmail.com>
2018-01-22 11:13:30 +0800

    added more scenes to show off level select

--- Assets/Scripts/SceneTransit.cs
+++ Assets/Scripts/SceneTransit.cs
@@ -2,14 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneTransit : MonoBehaviour {
 
+    public void RunLevel(Text levelID)
+    {
+        if (levelID.text == "Level 1")
+            SceneManager.LoadScene("Game");
+
+        else if (levelID.text == "Level 2")
+            SceneManager.LoadScene("Level2");
+
+        else if (levelID.text == "Level 3")
+            SceneManager.LoadScene("Level3");
+    }
 
 
     public void Game()
     {
         SceneManager.LoadScene("Game");
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene("Level3");
     }
 
     public void LevelSelect()
