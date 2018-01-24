ec4a1549b6fd4eeaf39d8399bfa9a9547f86a630
RJHere <greennexus51@gmail.com>
2018-01-24 10:35:57 +0800

    Main Menu and notifaction menu added

---
+++ Assets/Scripts/NotificationMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotificationMenu : MonoBehaviour {
+
+    public GameObject Panel1;
+
+    public void HidePanel()
+    {
+        Panel1.gameObject.SetActive(false);
+    }
+
+    public void ShowPanel()
+    {
+        Panel1.gameObject.SetActive(true);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
